16ecf12a88fd79ae4b6e95cad6f4b8644118eb69
Jorge Aguilar <aguila.techserv@gmail.com>
2020-05-21 10:12:10 -0500

    reactworks

---
+++ reactportfolio/src/App.js
@@ -0,0 +1,221 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   
+    <div>
+ <div class="container">
+  
+ <div class="card" >
+      <div class="card-body">
+        <h2>Portfolio</h2>
+       
+        <div class="row">
+          <div class="col-sm">
+           
+           
+            <h3> <a href="https://jaguilar1029531.github.io/prework-about-me/">About Me</a> </h3>
+            <h3> <a href="https://github.com/jaguilar1029531/prework-about-me">Github Code</a> </h3>
+            <p>This website is a static HTML website using semantic tags and minimal CSS.
+              It is currently hosted on Github Pages.</p>
+          </div>
+          <div class="col-sm">
+        
+       
+            <h3> <a href="https://jaguilar1029531.github.io/newhomework/">Code Refractor</a> </h3>
+            <h3> <a href="https://github.com/jaguilar1029531/newhomework">Github Code</a> </h3>
+            <p>The goal of this homework assignment is to: learn neat and organized syntax, consolidate code, improve accessibility to documents, improve functionality by making the code easier to read, delete useless stuff Changes I made to the HTML file I added a relevant title, whereas before it just read "website" line 8: I changed settings for the menu options, so that they all behaved similarly. One of the options didnt scroll down when clicked like it was supposed to, that works now. I added this by recognizing it from another piece of code: line 32: I added multiple photo alt attributes for accessibility standards I added multiple icon alt attributes for accessibility standards lines 34; 42; 50; 61; 69; 77 alt="photo of cluttered desk alt="photo of laptop in use alt="photo of team collaboration" alt="sales funnel icon" alt="lightbulb with wifi icon" alt="money signs icon" Changes I made to the CSS file Here I labeled sections which have similar info, so that it is easy to find info related to certain html sections I consolidated some lines of code into a single line Here is what I did in the CSS Noted purpose of lines number 11 through 189 removed 3 lines of code i felt i didnt' need on line 35</p>
+          </div>
+        </div>
+        <div class="row">
+            <div class="col-sm">
+            
+               
+                <h3> <a href="https://jaguilar1029531.github.io/HomeworkNumber2/homework2/">Portfolio</a> </h3>
+                <h3> <a href="https://github.com/jaguilar1029531/homework/tree/master/02-Homework">Github Code</a> </h3>
+                <p>THREE PAGE HTML HOMEWORK WORK DONE BY GEORGE AGUILAR
+
+                  THE GOAL OF THIS HOMEWORK WAS TO UTILIZE BOOTSTRAP AND TO MINIMIZE LOCAL CSS AND MEDIA QUERIES
+                  
+                  I FEEL LIKE I OVERCAME A LOT OF OBSTACLES AND MADE SOME GOOD BREAKTHROUGHS.
+                  
+                  CHANGES I MADE TO INDEX.HTML
+                  THE FIRST THING I DID WAS APPLY A LINK TO THE BOOTSTRAP WHERE I CAN GRAB ALL MY STYLES FROM
+                  
+                  I CHOSE A NAV BAR THAT LOOKED GOOD FROM BOOTSTRAP. BUT IT DIDNT FLOAT MY OPTIONS TO THE RIGHT LIKE IN THE EXAMPLE PHOTOS. SO I MADE A SECOND FLOAT-RIGHT STYLE NAVBAR AND PUT MY OPTIONS IN THERE SO THAT IT WOULD LOOK GOOD. I ALSO USED A BUTTON TO MAKE THE NAV DROPDOWN MENU ICON.
+                  
+                  TO OBTAIN SIMILAR UNDERLINE LINEBREAKS AS IN THE EXAMPLE PHOTO, I USED
+                  
+                  INSTEAD OF
+                  SO THAT I COULD GET A LINE BREAK WITH AN UNDERLINE
+                  TO GET THE PINK BOX AROUND THE PAGE HEADER I CREATED A CSS PIECE CALLED .DAGREENBOX AND GAVE IT WIDTH, BACKGROUND COLOR, PADDING, MARGIN AND TEXT + TEXT DECO
+                  
+                  FOR THE BODY I USED A SIMPLE CARD WITH A PHOTO PUT INSIDE OF IT, INSTEAD OF FORMATTING THE TEXT, I JUST FLOAT-LEFT THE PHOTO SO THAT THE TEXT WRAPS AROUND IT WHEN PUT INTO THE BOX. I PUT SOME PADDING AND TEXT FONT AND FAMILY IN THE CSS FOR THIS PART
+                  
+                  CHANGES I MADE TO CONTACT.HTML
+                  THE FIRST THING I DID WAS APPLY A LINK TO THE BOOTSTRAP WHERE I CAN GRAB ALL MY STYLES FROM
+                  
+                  I CHOSE A NAV BAR THAT LOOKED GOOD FROM BOOTSTRAP. BUT IT DIDNT FLOAT MY OPTIONS TO THE RIGHT LIKE IN THE EXAMPLE PHOTOS. SO I MADE A SECOND FLOAT-RIGHT STYLE NAVBAR AND PUT MY OPTIONS IN THERE SO THAT IT WOULD LOOK GOOD. I ALSO USED A BUTTON TO MAKE THE NAV DROPDOWN MENU ICON.
+                  
+                  TO OBTAIN SIMILAR UNDERLINE LINEBREAKS AS IN THE EXAMPLE PHOTO, I USED
+                  
+                  INSTEAD OF
+                  SO THAT I COULD GET A LINE BREAK WITH AN UNDERLINE
+                  TO GET THE PINK BOX AROUND THE PAGE HEADER I CREATED A CSS PIECE CALLED .DAGREENBOX AND GAVE IT WIDTH, BACKGROUND COLOR, PADDING, MARGIN AND TEXT + TEXT DECO
+                  
+                  FOR THE BODY I USED A DIVIDED CARD FORM PROVIDED TO ME FROM BOOTSTRAP. I HAD TO CHANGE A FEW OF THE WORDS TO MAKE IT LOOK LIKE I WANTED FOR SIMILARITY TO THE EXAMPLE PHOTO. THE FORMS ON BOOTSTRAP HAD DIFFERENT OPTIONS, AND I HAD TO CHANGE THE SIZE OF THE MESSAGE BOX IN THE CSS. THIS MAKES MESSAGES LOOK BETTER SO THAT YOU DON'T ONLY HAVE TO SEE A LITTLE BIT ON A LIMITED LINE AT A TIME
+                  
+                  CHANGES I MADE TO PORTFOLIO.HTML
+                  THE FIRST THING I DID WAS APPLY A LINK TO THE BOOTSTRAP WHERE I CAN GRAB ALL MY STYLES FROM
+                  
+                  I CHOSE A NAV BAR THAT LOOKED GOOD FROM BOOTSTRAP. BUT IT DIDNT FLOAT MY OPTIONS TO THE RIGHT LIKE IN THE EXAMPLE PHOTOS. SO I MADE A SECOND FLOAT-RIGHT STYLE NAVBAR AND PUT MY OPTIONS IN THERE SO THAT IT WOULD LOOK GOOD. I ALSO USED A BUTTON TO MAKE THE NAV DROPDOWN MENU ICON.
+                  
+                  TO OBTAIN SIMILAR UNDERLINE LINEBREAKS AS IN THE EXAMPLE PHOTO, I USED
+                  
+                  INSTEAD OF
+                  SO THAT I COULD GET A LINE BREAK WITH AN UNDERLINE
+                  TO GET THE PINK BOX AROUND THE PAGE HEADER I CREATED A CSS PIECE CALLED .DAGREENBOX AND GAVE IT WIDTH, BACKGROUND COLOR, PADDING, MARGIN AND TEXT + TEXT DECO
+                  
+                  FOR THE BODY I USED A CONTAINER CARD WITH A GRID. I TOOK THE STYLE FROM BOOTSTRAP, BUT IT WAS NOT QUITE AS LARGE AS I WANTED, SO I COPIED A PORTION OF THE CODE AND PASTED IT AGAIN TO GIVE ME 6 IMAGE WINDOWS INSTEAD OF 4. I PLAYED AROUND WITH WHERE TO PUT THE
+                  
+                  AND
+                  TAGS UNTIL I GOT IT TO A SPOT WHERE IT LOOKED NICE AND NEAT.
+                  
+                  CHANGES I MADE TO CSS
+                  I DID NOT MAKE MANY CHANGES HERE, I ADDED A MEDIA QUERY WHICH ALLOWS MY PAGES TO SHRINK WHEN THE WINDOW IS DOWNSIZED, AND I PUT STYLING FOR: A BOX, THE BODY OF ALL THREE DOCS, THE WIDTH OF ALL CARDS, THE HEIGHT OF THE MESSAGE BOX IN THE CONTACT FORM, AND THE MARGINS OF ALL THE IMAGES ON ALL DOCS</p>
+              </div>
+            <div class="col-sm">
+             
+                <h3> <a href="https://jaguilar1029531.github.io/0319homework/">Portfolio</a> </h3>
+                <h3> <a href="https://github.com/jaguilar1029531/0319homework">Github Code</a> </h3>
+                <p>The details of the goals are below. This was a very difficult assignment for me. I had to break down my understanding of javascript and reorganize it. I developed a love for console logging to see where my code is heading. I reached out to TAs, instructors in office hours, and many other students in an effort to find my way here. I spent 10 hours today studying to figure this out. What I've provided is a code that will accept user's input password criteria. It works if only some of the criteria is entered. It works if no criteria is entered, prompting the user to try again, and it works in denying usage of a number of characters not allowed. The code loops so that if the user fails to correctly input choices, it will run again. The code takes randomized characters from arrays and builds a password with them. Once the randomized password is created, the code prints that on the user's viewport. Although difficult, I had fun with this project, and enjoyed the mental breakthroughs of clarity that came along with it. Be sure to go through the viewport and click cancel on all the confirmations, thats my favorite.
+
+                  03 JavaScript: Password Generator
+                  Create an application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+                  
+                  If you are unfamiliar with special characters, take a look at some examples.
+                  
+                  User Story
+                  AS AN employee with access to sensitive data
+                  I WANT to randomly generate a password that meets certain criteria
+                  SO THAT I can create a strong password that provides greater security
+                  Acceptance Criteria
+                  GIVEN I need a new, secure password
+                  WHEN I click the button to generate a password
+                  THEN I am presented with a series of prompts for password criteria
+                  WHEN prompted for password criteria
+                  THEN I select which criteria to include in the password
+                  WHEN prompted for the length of the password
+                  THEN I choose a length of at least 8 characters and no more than 128 characters
+                  WHEN prompted for character types to include in the password
+                  THEN I choose lowercase, uppercase, numeric, and/or special characters
+                  WHEN I answer each prompt
+                  THEN my input should be validated and at least one character type should be selected
+                  WHEN all prompts are answered
+                  THEN a password is generated that matches the selected criteria
+                  WHEN the password is generated
+                  THEN the password is either displayed in an alert or written to the page</p>
+              </div>
+          </div>
+          <div class="row">
+            <div class="col-sm">
+            
+                <h3> <a href="https://jaguilar1029531.github.io/newTimedQuizHW/">Timed Quiz</a> </h3>
+                <h3> <a href="https://github.com/jaguilar1029531/newTimedQuizHW">Github Code</a> </h3>
+                <p>newTimedQuizHW
+                  I messed up and deleted my last repository because it got too entangled with mistakes, but I have a screenshot that shows I made 13 commits before creating this one
+                  
+                  i've never had to do so much google-fu in my life, and yes this is not complete but i wanted to turn in something. I still need a minus 5 second timer for wrong answers and a high score card, neither of those seem too difficult and i will get on them asap
+                  
+                  in this homework assignment i was to create a multiple choice quiz with a timer
+                  
+                  I created my own questions mostly and appended them to the html doc
+                  
+                  with some buttons that activated when i pressed them to let me move around the quiz and submit answers</p>
+              </div>
+            <div class="col-sm">
+            
+                <h3> <a href="https://jaguilar1029531.github.io/calendarhw/">Daily Planner</a> </h3>
+                <h3> <a href="https://github.com/jaguilar1029531/calendarhw">Github Code</a> </h3>
+                <p>05 Third-Party APIs: Work Day Scheduler
+                  Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+                  
+                  You'll need to use the Moment.js library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
+                  
+                  User Story
+                  AS AN employee with a busy schedule
+                  I WANT to add important events to a daily planner
+                  SO THAT I can manage my time effectively
+                  Acceptance Criteria
+                  GIVEN I am using a daily planner to create a schedule
+                  WHEN I open the planner
+                  THEN the current day is displayed at the top of the calendar
+                  WHEN I scroll down
+                  THEN I am presented with timeblocks for standard business hours
+                  WHEN I view the timeblocks for that day
+                  THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+                  WHEN I click into a timeblock
+                  THEN I can enter an event
+                  WHEN I click the save button for that timeblock
+                  THEN the text for that event is saved in local storage
+                  WHEN I refresh the page
+                  THEN the saved events persist</p>
+              </div>
+              <div class="col-sm">
+               
+                  <h3> <a href="https://jaguilar1029531.github.io/weatherhw/">Weather App</a> </h3>
+                  <h3> <a href="https://github.com/jaguilar1029531/weatherhw">Github Code</a> </h3>
+                  <p>weatherhw
+                    welcome to my weather dashboard
+                    
+                    I am still working out some bugs, so if you see this message, I am still working on it. I just wanted to turn something in, thank you.</p>
+                </div>
+                <div class="col-sm">
+               
+                    <h3> <a href="http://htmlpreview.github.io/?https://github.com/robertrcandler/BootCampProject1/blob/master/Website/main.html">Texas Tiger Finder</a> </h3>
+                    <h3> <a href="https://github.com/robertrcandler/BootCampProject1">Github Code</a> </h3>
+                    <p>Unit 7: Project 1
+                      Overview
+                      Welcome to Project 1! This is an important step in your full-stack journey as you will now use your newly acquired skills and knowledge to build web application from scratch. With your team, you will conceive and execute a design that solves a real-world problem by integrating data received from multiple server-side API requests. Because you will be working collaboratively, you will learn agile development methodologies and implement feature and bug fixes using the git branch workflow and pull requests.
+                      
+                      Key Topics
+                      Server-side APIs
+                      Git branching workflow
+                      Agile software development
+                      Comprehension Check
+                      You will be employer-ready if you can answer the following questions:
+                      
+                      What is agile software development?
+                      What is an MVP?
+                      If you pull down a teammate's branch and you have merge conflicts, how do you handle it?
+                      Learning Objectives
+                      You will be employer-competitive if you are able to:
+                      
+                      Speak technically about a feature you implemented in your project
+                      Explain and execute git branching workflow in a collaborative project
+                      Resolve merge conflicts
+                      Explain agile software development
+                      Design, build, and deploy a client-side web application using GitHub Pages
+                      Prepare a professional presentation and repository README for your project</p>
+                  </div>
+              
+          </div>
+      </div>
+    </div>
+    
+</div>
+    </div>
+
+         
+ 
+ 
+
+    
+    
+  
+  );
+}
+
+export default App;
